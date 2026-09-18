51c07e763217c410016e09908d8b01cd8574a18a
JesReyA <jesreyav@gmail.com>
2026-09-17 20:55:18 -0600

    Cambios para solucionar puerto

---
+++ src/test/java/com/tuempresa/empresa_backend/src/test/java/com/tuempresa/empresa_backend/EmpresaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuempresa.empresa_backend.src.test.java.com.tuempresa.empresa_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
